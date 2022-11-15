e0cb7707719737dcaf4d3ccb5d5903a969f6cf9c
Roboskeletron <wwwrt287@gmail.com>
2022-11-15 23:34:17 +0300

    main implemented

--- src/task9/console/Main.java
+++ src/task9/console/Main.java
@@ -1,7 +1,22 @@
 package task9.console;
 
+import task9.ArrayIO;
+import task9.WeightCalculator;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        FileInputStream inputStream = new FileInputStream(args[0]);
+        FileOutputStream outputStream = new FileOutputStream(args[1]);
+        int weightSum = Integer.getInteger(args[2]);
+
+        var weights = ArrayIO.getArray(inputStream);
+
+        var variants = WeightCalculator.getVariants(weights, weightSum);
+
+        System.out.println(ArrayIO.saveArray(outputStream, variants, true));
     }
 }
