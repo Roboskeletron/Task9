a999722a496b5e55a85a55328b8afda197c70211
Roboskeletron <wwwrt287@gmail.com>
2022-11-16 22:26:37 +0300

    MainForm created

---
+++ src/task9/jtable/MainForm.java
@@ -0,0 +1,10 @@
+package task9.jtable;
+
+import javax.swing.*;
+
+public class MainForm extends JFrame {
+    public MainForm(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pack();
+    }
+}
