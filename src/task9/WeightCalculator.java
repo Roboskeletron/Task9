f7d462c15189801b9c665f907dc4de92f6bde235
Roboskeletron <wwwrt287@gmail.com>
2022-11-17 00:39:37 +0300

    isInputValid implemented and it`s call added to getVariants

--- src/task9/WeightCalculator.java
+++ src/task9/WeightCalculator.java
@@ -1,10 +1,16 @@
 package task9;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.List;
 
 public class WeightCalculator {
     public static List<List<Integer>> getVariants(Integer[] weights, int weightSum) {
+        if (!isInputValid(weights))
+            throw new InvalidParameterException("You have more than 10 weights or 2 or more same weights");
+
         List<List<Integer>> variants = new ArrayList<>();
 
         for (int variant = 0; variant < Math.pow(2, weights.length); variant++) {
@@ -32,5 +38,20 @@ public class WeightCalculator {
         }
 
         return variant;
+    }
+
+    public static boolean isInputValid(Integer[] weights){
+        if (weights.length > 10)
+            return false;
+
+        Dictionary<Integer, Integer> weightsStatistics = new Hashtable<>();
+
+        for (var weight : weights){
+            if (weightsStatistics.get(weight) == null)
+                weightsStatistics.put(weight, 1);
+            else return false;
+        }
+
+        return true;
     }
 }
