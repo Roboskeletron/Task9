1e56c63a1c3e9b8ebededaed484b05e881bd512e
Roboskeletron <wwwrt287@gmail.com>
2022-11-16 23:43:53 +0300

    deleteRow and getDataFromVector implemented

--- src/task9/jtable/WeightsListTableModel.java
+++ src/task9/jtable/WeightsListTableModel.java
@@ -22,4 +22,29 @@ public class WeightsListTableModel extends DefaultTableModel {
 
         setDataVector(dataVector, new String[]{"Weights"});
     }
+
+    public void addRow() {
+        if (dataVector.size() < 10) {
+            Integer[] row = new Integer[]{0};
+            addRow(row);
+            return;
+        }
+
+        throw new RuntimeException("You already have 10/10 weights!");
+    }
+
+    public void deleteRow() {
+        if (dataVector.size() > 0)
+            dataVector.remove(dataVector.size() - 1);
+    }
+
+    public Integer[] getDataFromVector(){
+        Integer[] data = new Integer[dataVector.size()];
+
+        for (int i = 0; i < data.length; i++){
+            data[i] = (Integer) dataVector.get(i).get(0);
+        }
+
+        return data;
+    }
 }
