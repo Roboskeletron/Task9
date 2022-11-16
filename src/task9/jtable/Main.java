6710bc92aa8202a55a9d2999c4714429ab1f1e32
Roboskeletron <wwwrt287@gmail.com>
2022-11-16 22:26:27 +0300

    MainForm initialized

--- src/task9/jtable/Main.java
+++ src/task9/jtable/Main.java
@@ -1,7 +1,9 @@
 package task9.jtable;
 
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args){
-
+        EventQueue.invokeLater(() -> new MainForm().setVisible(true));
     }
 }
